bd8099e318c07144d3398668b3a2330c2934eb99
sahithi442-git <2211cs010442@mallareddyuniversity.ac.in>
2025-01-09 19:33:42 +0530

    Add files via upload

---
+++ fun-declaration.js
@@ -0,0 +1,46 @@
+//function demo(){
+    //console.log*("hello-world")
+
+//}
+//demo()
+
+//function sum(a, b)
+//{
+    // let c= a+b;
+     //return c;
+    
+//}
+//console.log(sum(10, 10));
+
+//function arithmeticOperations(a, b) {
+    
+    //const results = {
+       // addition: a + b,
+        //subtraction: a - b,
+        //multiplication: a * b,
+        //division: b !== 0 ? a / b : "Undefined (division by zero)"
+    //};
+
+    //return results;
+//}
+
+
+const num1 = 10;
+const num2 = 5;
+const output = arithmeticOperations(num1, num2);
+console.log(output);
+function operator()
+{
+    let a= Number(window.prompt("enter a  value"))
+    let b= Number(window.prompt("enter b value"))
+    let c= a+b
+    let d= a-b
+    let e= a*b;
+    console.log(c)
+    console.log(d)
+    console.log(e);
+}
+
+
+
+
