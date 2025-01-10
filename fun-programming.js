50fb6805b58cde2a934e0d061d2f924bcf3e2b8c
sahithi442-git <2211cs010442@mallareddyuniversity.ac.in>
2025-01-10 13:15:46 +0530

    Add files via upload

---
+++ fun-programming.js
@@ -0,0 +1,39 @@
+//function main(a)
+//{
+//console.log(a);
+//console.log(a());
+//}
+//main(function(){
+  //  return "iam cbf1"
+//})
+//main(function(){
+  //  return "iam sai"
+//})
+//main(function(){
+  //  return "iam sahithi"
+//})
+
+
+//function main1(task)
+//{
+  //  console.log(task(10,10));
+
+//}
+//main1(function(a, b){
+  //  return a+b
+//})
+//main1(function(a,  b){
+  //  return a-b
+    
+//})
+//main1(function(a, b){
+  //  return a*b
+//})
+
+
+let a = () => console.log("hello");
+console.log(a);
+a()
+ let b= () => {return "hello"}
+ console.log(b);
+ console.log(b());
