e03529ea3d877d002173cc259f922c5b5bbdae7d
sahithi442-git <2211cs010442@mallareddyuniversity.ac.in>
2025-01-24 11:41:19 +0530

    Add files via upload

---
+++ evenrt.js
@@ -0,0 +1,27 @@
+//events in dom
+//--> it is an action performed by end-user on a web-page
+
+//-->types
+
+//--> 1. key-board event
+         //key-up
+         //key-down
+         //key-press
+
+//--> 2. mouse event
+         //onmouseover
+         //onmouseleave
+         //dbclick
+         
+//--> 3. form event
+         //input
+         //focus
+         //key-press
+
+//--> 4. pointer event
+        //click-event
+
+
+        function event1(){
+            console.log("hello-world")
+        }
